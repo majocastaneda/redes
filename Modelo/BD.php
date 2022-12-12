a3eccee8221b03ce54ca6c33dc0ce1b7ba68fb8e
majocastaneda <maria.castaneda@trade.ec>
2022-12-12 11:11:35 -0500

    localhost

--- Modelo/BD.php
+++ Modelo/BD.php
@@ -6,7 +6,7 @@ function Conectar() {
     echo '-------------------';
 echo 'antes de conectar a la bd';
 echo '-------------------';
-    $link = new mysqli("192.168.100.59:3307", "root", "Majocast124!", "libreria") or die("Error al conectar a la B.D");
+    $link = new mysqli("localhost:3307", "root", "Majocast124!", "libreria") or die("Error al conectar a la B.D");
     echo '-------------------';
 echo 'el valor de la conexion es:'.$link;
 echo '-------------------';
