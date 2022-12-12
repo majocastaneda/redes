8fca8015a36ddfd6f1e91c481e38d385af1f795e
majocastaneda <maria.castaneda@trade.ec>
2022-12-12 11:03:08 -0500

    cambio puerto 3307

--- Modelo/BD.php
+++ Modelo/BD.php
@@ -6,7 +6,7 @@ function Conectar() {
     echo '-------------------';
 echo 'antes de conectar a la bd';
 echo '-------------------';
-    $link = new mysqli("192.168.100.59", "majocast", "Majocast124!", "libreria") or die("Error al conectar a la B.D");
+    $link = new mysqli("192.168.100.59:3307", "majocast", "Majocast124!", "libreria") or die("Error al conectar a la B.D");
     echo '-------------------';
 echo 'el valor de la conexion es:'.$link;
 echo '-------------------';
